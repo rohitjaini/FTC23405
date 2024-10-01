c61f2b1327e402ba529dfad562e64ee61afd017a
oobeegamer <ritvikkr@outlook.com>
2024-09-30 21:07:43 -0700

    LM0 Code - Not Working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CORobotCodeV6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/CORobotCodeV6.java
@@ -10,28 +10,25 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.teamcode.kdrobot.DriveTrainSpeed;
-import org.firstinspires.ftc.teamcode.kdrobot.KDRobot;
 
 @TeleOp
 public class CORobotCodeV6 extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
-        KDRobot robot = new KDRobot();
-        robot.init(hardwareMap, telemetry);
         // Make sure your ID's match your configuration
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
-        DcMotor rightSlideMotor = hardwareMap.get(DcMotor.class, "RightLS");
-        DcMotor leftSlideMotor = hardwareMap.get(DcMotor.class, "LeftLS");
-        Servo rightWristServo = hardwareMap.get(Servo.class, "aHO");
-        Servo leftWristServo = hardwareMap.get(Servo.class, "aHT");
-        Servo rightClawServo = hardwareMap.get(Servo.class, "cO");
-        Servo leftClawServo = hardwareMap.get(Servo.class, "cT");
-        DcMotor intakeArmMotor = hardwareMap.get(DcMotor.class, "aB");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        DcMotor rightSlideMotor = hardwareMap.get(DcMotor.class, "rightSlideMotor");
+        DcMotor leftSlideMotor = hardwareMap.get(DcMotor.class, "leftSlideMotor");
+        Servo rightWristServo = hardwareMap.get(Servo.class, "rightWristServo");
+        Servo leftWristServo = hardwareMap.get(Servo.class, "leftWristServo");
+        Servo rightClawServo = hardwareMap.get(Servo.class, "rightClawServo");
+        Servo leftClawServo = hardwareMap.get(Servo.class, "leftClawServo");
+        DcMotor intakeArmMotor = hardwareMap.get(DcMotor.class, "intakeArmMotor");
+        Servo bucketServo = hardwareMap.get(Servo.class, "bucketServo");
         intakeArmMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -40,17 +37,23 @@ public class CORobotCodeV6 extends LinearOpMode {
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
         // See the note about this earlier on this page.
-        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
@@ -59,8 +62,8 @@ public class CORobotCodeV6 extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_x; // Remember, Y stick value is reversed
-            double x = -gamepad1.left_stick_y * 1.1; // Counteract imperfect strafing
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
             // This button choice was made so that it is hard to hit on accident,
@@ -86,7 +89,6 @@ public class CORobotCodeV6 extends LinearOpMode {
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
-            double driveTrainSpeed = 0.1;
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
@@ -101,37 +103,44 @@ public class CORobotCodeV6 extends LinearOpMode {
                         leftSlideMotor.setTargetPosition(0);
                         // Linear Slide movement End
 
-                        if (gamepad1.right_bumper) {
-                            sleep(250);
-                            robot.changeDriveTrainSpeed(DriveTrainSpeed.INCREASE);
-                        }
-                        if (gamepad1.left_bumper) {
-                            sleep(250);
-                            robot.changeDriveTrainSpeed(DriveTrainSpeed.DECREASE);
-
                             if (gamepad1.left_trigger == 1) {
+                                telemetry.addLine("lefttrigger pressed");
                                 rightClawServo.setPosition(1);
                                 leftClawServo.setPosition(1);
                             if (gamepad1.right_trigger == 1) {
+                                telemetry.addLine("righttrigger pressed");
                                 rightClawServo.setPosition(0);
                                 leftClawServo.setPosition(0);
 
                                         if (gamepad1.x) {
-                                            intakeArmMotor.setTargetPosition(-1);
+                                            telemetry.addLine("x pressed");
+                                            rightWristServo.setPosition(-0.25);
+                                            leftWristServo.setPosition(-0.25);
                                             sleep(200);
-                                            rightWristServo.setPosition(-0.5);
-                                            leftWristServo.setPosition(-0.5);
+                                            intakeArmMotor.setTargetPosition(-1);
+
 
                                             if (gamepad1.a) {
+                                                telemetry.addLine("a pressed");
                                                 intakeArmMotor.setTargetPosition(0);
-                                                rightWristServo.setPosition(1);
-                                                leftWristServo.setPosition(1);
+                                                sleep(200);
+                                                rightWristServo.setPosition(0.25);
+                                                leftWristServo.setPosition(0.25);
 
+                                                if (gamepad1.y) {
+                                                    telemetry.addLine("y pressed");
+                                                    bucketServo.setPosition(-0.5);
+                                                }
+
+                                                if (gamepad1.b) {
+                                                    telemetry.addLine("b pressed");
+                                                    bucketServo.setPosition(0);
+                                                }
                                         }
                                     }
                                 }
                             }
-                            }
+                        }
                     }
                 }
 
@@ -145,9 +154,3 @@ public class CORobotCodeV6 extends LinearOpMode {
 
                         }
                     }
-                }
-            }
-        }
-    }
-}
-
